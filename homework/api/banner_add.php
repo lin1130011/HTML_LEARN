62163e0a77feb87abc5a9f5b226eb3bb550a82ca
unknown <lin1130011@gmail.com>
2024-08-04 20:04:19 +0800

    初步完成且更新資料表名稱

--- homework/api/banner_add.php
+++ homework/api/banner_add.php
@@ -1,11 +1,12 @@
 <?php
 include_once "./base.php";
-
+dd($_FILES);
+dd($_POST);
 $Banner = new DB('ck_banner');
 if (!empty($_FILES['img']['tmp_name'])) {
     move_uploaded_file($_FILES['img']['tmp_name'], "../images/banner/" . $_FILES['img']['name']);
     $_POST['img'] = $_FILES['img']['name'];
     dd($_POST);
     $Banner->store($_POST);
+    header("location:../admin.php?do=banner");
 }
-header("location:../admin.php?do=banner");
